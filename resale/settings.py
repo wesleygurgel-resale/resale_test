f9aa4733f32ca33abf3ebd6956dd6ec5fd7e83f8
wesleygurgel <wesleygurgel27@gmail.com>
2021-08-21 15:28:23 -0300

    (DB): Back to DB_SQLITE3

--- resale/settings.py
+++ resale/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'resale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DEMO_TEST',
@@ -91,7 +91,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432'
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
