c5ad45e48c39d3ffb088318ea78e9d034adcebc4
wesleygurgel <wesleygurgel27@gmail.com>
2021-08-21 16:20:07 -0300

    (FEAT): Filter by 'nome'

---
+++ imoveis/filters.py
@@ -0,0 +1,15 @@
+from django_filters import rest_framework as filters
+
+from .models import Imovel, Imobiliaria
+
+
+class ImobiliariaFilter(filters.FilterSet):
+    class Meta:
+        model = Imobiliaria
+        fields = ['nome']
+
+
+class ImovelFilter(filters.FilterSet):
+    class Meta:
+        model = Imovel
+        fields = ['nome']
--- imoveis/views.py
+++ imoveis/views.py
@@ -1,5 +1,6 @@
 from .models import Imovel, Imobiliaria
 from .serializers import ImovelSerializer, ImobiliariaSerializer
+from .filters import ImovelFilter, ImobiliariaFilter
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -14,10 +15,11 @@ API V1
 class ImobiliariaViewSet(viewsets.ModelViewSet):
     queryset = Imobiliaria.objects.all()
     serializer_class = ImobiliariaSerializer
+    filterset_class = ImobiliariaFilter
 
     @action(detail=True, methods=['GET'])
     def listar_imoveis(self, request, pk=None):
-        self.pagination_class.page_size = 1
+        self.pagination_class.page_size = 2
         imoveis = Imovel.objects.filter(imobiliaria_id=pk)
         page = self.paginate_queryset(imoveis)
 
@@ -38,3 +40,4 @@ class ImovelViewSet(mixins.CreateModelMixin,
                     ):
     queryset = Imovel.objects.all()
     serializer_class = ImovelSerializer
+    filterset_class = ImovelFilter
