bc74546178f48cd22975fcb0786777799d99abf3
wesleygurgel <wesleygurgel27@gmail.com>
2021-08-21 15:12:50 -0300

    (RELEASE): Configure Requirements and Settings to start deploy.

--- resale/settings.py
+++ resale/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'resale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'wesley',
+        'PASSWORD': '71txkq',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
