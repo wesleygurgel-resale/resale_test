3109ed4b5d06982e5eb0d4632b07f5d7662acbf5
wesleygurgel <wesleygurgel27@gmail.com>
2021-08-24 10:46:15 -0300

    Removed unusable things + migrations

--- resale/settings.py
+++ resale/settings.py
@@ -58,14 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'resale.wsgi.application'
 
-
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}"""
-
 # DB LOCAL
 DATABASES = {
     'default': {
